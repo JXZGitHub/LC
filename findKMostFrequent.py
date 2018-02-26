21fbfdcaeef633d9700ce4425afd18149fb362dd
JXZGitHub <joe.zhang@gmail.com>
2018-02-25 22:25:36 -0500

    enhanced.

--- findKMostFrequent.py
+++ findKMostFrequent.py
@@ -6,17 +6,37 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
+        count = collections.Counter(nums)
+        freqs = [ [] for _ in range(len(nums)+1)]
+        res = []
+        n = len(nums)
+        for num, freq in count.items():
+            freqs[freq].append(num)
+        while k:
+            while not freqs[n]:
+               n -= 1
+            res.append(freqs[n].pop())
+            k -= 1
+        return res
+
+    def topKFrequent2(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
         n = len(nums)
         cntDict = collections.defaultdict(int)
         for i in nums:
             cntDict[i] += 1
         freqList = [[] for i in range(n + 1)] #Each sublist is the list of nums that occured X number of times, where X is the index position the sublist in the outer list
         for p in cntDict:
-            freqList[cntDict[p]] += p #If there's more than 1 num that appeared same number of times, append the numbers
+            freqList[cntDict[p]].append(p) #If there's more than 1 num that appeared same number of times, append the numbers
         ans = []
         for p in range(n, 0, -1):
-            ans += freqList[p]
+            ans += freqList[p] #same as ans.extend(freqList[p]), so emtpy list [] doens't end up being added to ans.
         return ans[:k]
 
 sol = Solution()
-print (sol.topKFrequent([1,1,1,2,2,3],2))
+#print (sol.topKFrequent([1,1,1,2,2,3],2))
+print (sol.topKFrequent2([1,2,3],2))
