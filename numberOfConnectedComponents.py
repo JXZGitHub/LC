a54fdc5e6e7728c7d9f8ed5172a66496e944c513
JXZGitHub <joe.zhang@gmail.com>
2018-09-18 21:17:18 -0400

    First version.

--- numberOfConnectedComponents.py
+++ numberOfConnectedComponents.py
@@ -1,46 +1,71 @@
-def cc(g):
-    p={}
-    r={}    
-    for (a,b) in g:
-        print 'procesing', a, b
-        pA=find(a,p)
-        pB=find(b,p)   
-        if pA!=pB:
-            res -= 1
-            ##Union by rank , parent (set) with smaller rank joins bigger rank.
-            rA=r.setdefault(pA,0)
-            rB=r.setdefault(pB,0)
-            if rA == rB:
-                r[pB] = rB+1 #Same ranks joined, resulting set has +1 rank.
-            elif rA>rB: #Ensures smaller rank joins bigger rank.
-                pA,pB=pB,pA
+import collections
+class Solution_DFS:
+    def countComponents(self, n, edges):
+        """
+            :type n: int
+            :type edges: List[List[int]]
+            :rtype: int
 
-            print 'setting ' + str(pA) + 's parent to be ' + str(pB)
-            p[pA]=p[pB]
-    res = set()
+            Time: O(N+E)
+            Space: Stack O(max # of connected edges). Heap O(N)
+        """
+        graph = collections.defaultdict(list)
+        visited = set()
+        res = 0
+        for (a, b) in edges:
+            graph[a].append(b)
+            graph[b].append(a)
 
-    for n in p.keys():
-        res.add(find(n,p))
- 
-    print res,p
-    print len(res)
+        for node in range(n):
+            if node not in visited:
+                res += 1
+                self.traverse(node, graph, visited)
+        return res
 
-def cc2(g):
-    p=collections.defaultdict(list)
-    v=set()
-    res = 0
-    for (a,b) in g:        
-        p[a].append(b)
-        p[b].append(a)
-    for n in p:
-        if n not in v:
-            res +=1
-            rec(n,p,v)
-    print res
+    def traverse(self, n, graph, visited):
+        if n in visited:
+            return
+        visited.add(n)
+        for child in graph.get(n, []):
+            self.traverse(child, graph, visited)
 
-def rec(n,p,v):
-    if n in v:
-        return
-    v.add(n)
-    for c in p[n]:
-        rec(c,p,v) 
+
+class Solution_UF:
+    def countComponents(self, n, edges):
+        """
+            :type n: int
+            :type edges: List[List[int]]
+            :rtype: int
+
+            Time: O(N)
+            Space: O(N)
+        """
+        parents = {}
+        rank = {}
+        for (a, b) in edges:
+            pA = self.find(a, parents)
+            pB = self.find(b, parents)
+            if pA != pB:
+                ##Union by rank , parent (set) with smaller rank joins bigger rank.
+                rA = rank.setdefault(pA, 0)
+                rB = rank.setdefault(pB, 0)
+                if rA == rB:
+                    rank[pB] = rB + 1  # Two sets with same ranks joined, resulting set has +1 rank.
+                elif rA > rB:  # Ensures that smaller rank joins bigger rank.
+                    pA, pB = pB, pA
+
+                parents[pA] = parents[pB]
+        res = set()
+
+        for node in range(n):
+            res.add(self.find(node, parents))
+
+        return len(res)
+
+    def find(self, node, parents):
+        if node not in parents:
+            parents[node] = node
+            return node
+        if parents[node] != node:
+            parents[node] = self.find(parents[node], parents)
+        return parents[node]
